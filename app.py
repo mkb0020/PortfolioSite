b006255feaf8774d30c4565efdb463fd801796a9
MK Barriault <MK.BARRIAULT@outlook.com>
2025-10-25 13:39:44 -0400

    host 0 0 0 0

--- app.py
+++ app.py
@@ -283,4 +283,5 @@ def order_confirmation():
     return render_template("order_confirmation.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
