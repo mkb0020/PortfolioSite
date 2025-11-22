d6b5f21e9466fecb079009e2fd10ec787ac63a18
MK Barriault <MK.BARRIAULT@outlook.com>
2025-11-22 08:57:30 -0500

    Characters for CATastrophe

---
+++ static/js/CATastrophe/FPS/characters.js
@@ -0,0 +1,135 @@
+// characters.js
+export const CHARACTERS = {
+  NONA: {
+    name: 'NONA',
+    baseHP: 120,
+    baseAtk: 42,
+    baseSpeed: 38,
+    baseDefense: 30,
+    moves: {
+      "SCRATCH": { dmg: 24, uses: 5 },
+      "CATNIP CLAW": { dmg: 42, uses: 2 },
+      "MAKE BISCUITS": { heal: 30, uses: 3 },
+      "ZOOMIES": { dmg: 25, speedBoost: true, uses: 3 }
+    },
+    sprites: {
+      small: 'NonaImg.png',
+      big: 'NonaBigImg.png',
+      // LAZY LOAD THESE
+      walk: 'nonaWalk.png',
+      jump: 'nonaJump.png',
+      stand: 'nonaStand.png'
+    }
+  },
+
+  GATO: {
+    name: 'GATO',
+    baseHP: 120,
+    baseAtk: 42,
+    baseSpeed: 38,
+    baseDefense: 30,
+    moves: {
+      "SCRATCH": { dmg: 24, uses: 5 },
+      "CATNIP CLAW": { dmg: 42, uses: 2 },
+      "MAKE BISCUITS": { heal: 30, uses: 3 },
+      "ZOOMIES": { dmg: 25, speedBoost: true, uses: 3 }
+    },
+    sprites: {
+      small: 'GatoImg.png',
+      big: 'GatoBigImg.png',
+      // LAZY LOAD THESE
+      walk: 'GatoWalk.png',
+      jump: 'GatoJump.png',
+      stand: 'GatoStand.png'
+    }
+  },
+
+  NIELS: {
+    name: 'NIELS',
+    baseHP: 120,
+    baseAtk: 42,
+    baseSpeed: 38,
+    baseDefense: 30,
+    moves: {
+      "SCRATCH": { dmg: 24, uses: 5 },
+      "CATNIP CLAW": { dmg: 42, uses: 2 },
+      "MAKE BISCUITS": { heal: 30, uses: 3 },
+      "ZOOMIES": { dmg: 25, speedBoost: true, uses: 3 }
+    },
+    sprites: {
+      small: 'NielsImg.png',
+      big: 'NielsBigImg.png',
+      // LAZY LOAD THESE
+      walk: 'NielsWalk.png',
+      jump: 'NielsJump.png',
+      stand: 'NielsStand.png'
+    }
+  },
+
+  NOVA: {
+    name: 'NOVA',
+    baseHP: 120,
+    baseAtk: 42,
+    baseSpeed: 38,
+    baseDefense: 30,
+    moves: {
+      "SCRATCH": { dmg: 24, uses: 5 },
+      "CATNIP CLAW": { dmg: 42, uses: 2 },
+      "MAKE BISCUITS": { heal: 30, uses: 3 },
+      "ZOOMIES": { dmg: 25, speedBoost: true, uses: 3 }
+    },
+    sprites: {
+      small: 'NovaImg.png',
+      big: 'NovaBigImg.png',
+      // LAZY LOAD THESE
+      walk: 'NovaWalk.png',
+      jump: 'NovaJump.png',
+      stand: 'NovaStand.png'
+    }
+  },
+
+  CLOUDY: {
+    name: 'CLOUDY',
+    baseHP: 120,
+    baseAtk: 42,
+    baseSpeed: 38,
+    baseDefense: 30,
+    moves: {
+      "SCRATCH": { dmg: 24, uses: 5 },
+      "CATNIP CLAW": { dmg: 42, uses: 2 },
+      "MAKE BISCUITS": { heal: 30, uses: 3 },
+      "ZOOMIES": { dmg: 25, speedBoost: true, uses: 3 }
+    },
+    sprites: {
+      small: 'CloudyImg.png',
+      big: 'CloudyBigImg.png',
+      // LAZY LOAD THESE
+      walk: 'CloudyWalk.png',
+      jump: 'CloudyJump.png',
+      stand: 'CloudyStand.png'
+    }
+  },
+
+  DOUG: {
+    name: 'DOUG',
+    baseHP: 120,
+    baseAtk: 42,
+    baseSpeed: 38,
+    baseDefense: 30,
+    moves: {
+      "SCRATCH": { dmg: 24, uses: 5 },
+      "CATNIP CLAW": { dmg: 42, uses: 2 },
+      "MAKE BISCUITS": { heal: 30, uses: 3 },
+      "ZOOMIES": { dmg: 25, speedBoost: true, uses: 3 }
+    },
+    sprites: {
+      small: 'DougImg.png',
+      big: 'DougBigImg.png',
+      // LAZY LOAD THESE
+      walk: 'DougWalk.png',
+      jump: 'DougJump.png',
+      stand: 'DougStand.png'
+    }
+  },
+  // ... other cats
+};
